30e57d44e5613aaea7f001a5b023ac3b4529521b
Kazooy <davidok7@gmail.com>
2015-05-04 15:35:59 +1000

    new

---
+++ Stuff/turingcreator.py
@@ -0,0 +1,22 @@
+import kivy
+kivy.require('1.7.0')
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager
+
+Builder.load_file('toolbox.kv')
+Builder.load_file('turingwidgets.kv')
+Builder.load_file('drawingspace.kv')
+Builder.load_file('generaloptions.kv')
+Builder.load_file('statusbar.kv')
+Builder.load_file('turingcreator.kv')
+
+class TuringScreenManager(ScreenManager):
+    pass
+
+class TuringScreenManagerApp(App):
+    def build(self):
+        return TuringScreenManager()
+
+if __name__=="__main__":
+    TuringScreenManagerApp().run()
